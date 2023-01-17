0bb92061bf3245ccb07c3677d62b5e60701c3c07
ericchase <github@eric.addymail.com>
2023-01-16 20:35:33 -0600

    formatting
    Signed-off-by: ericchase <github@eric.addymail.com>

--- src/components/AnimeList/Pagination.jsx
+++ src/components/AnimeList/Pagination.jsx
@@ -1,43 +1,43 @@
 import { Link } from "react-router-dom";
 import style from "./Pagination.module.css";
 const Pagination = ({ pagination, url }) => {
-	const { items, current_page, last_visible_page } = pagination;
+  const { items, current_page, last_visible_page } = pagination;
 
-	let paginationLinks = [];
+  let paginationLinks = [];
 
-	function addLink(page) {
-		const className = page === current_page ? style["current"] : "";
-		paginationLinks.push(
-			<li key={page}>
-				<Link to={`${url}?page=${page}`} className={className}>
-					{page}
-				</Link>
-			</li>,
-		);
-	}
+  function addLink(page) {
+    const className = page === current_page ? style["current"] : "";
+    paginationLinks.push(
+      <li key={page}>
+        <Link to={`${url}?page=${page}`} className={className}>
+          {page}
+        </Link>
+      </li>,
+    );
+  }
 
-	// sliding window?
-	const pad = 2;
-	let [beg, end] = [2, 2 + pad + pad];
-	for (let i = beg; i + pad + pad < last_visible_page - 1; ++i) {
-		console.log(beg, i, end);
-		if (current_page <= beg + pad) break;
-		++beg;
-		++end;
-	}
+  // sliding window?
+  const pad = 2;
+  let [beg, end] = [2, 2 + pad + pad];
+  for (let i = beg; i + pad + pad < last_visible_page - 1; ++i) {
+    console.log(beg, i, end);
+    if (current_page <= beg + pad) break;
+    ++beg;
+    ++end;
+  }
 
-	addLink(1);
-	/* beg separator */ paginationLinks.push(<span key="sepl">...</span>);
-	for (let i = beg; i <= end; i++) {
-		addLink(i);
-	}
-	/* end separator */ paginationLinks.push(<span key="sepr">...</span>);
-	addLink(last_visible_page);
+  addLink(1);
+  /* beg separator */ paginationLinks.push(<span key="sepl">...</span>);
+  for (let i = beg; i <= end; i++) {
+    addLink(i);
+  }
+  /* end separator */ paginationLinks.push(<span key="sepr">...</span>);
+  addLink(last_visible_page);
 
-	return (
-		<nav aria-label="pagination" className="nav-pagination">
-			<ul className={style["pagination"]}>{paginationLinks}</ul>
-		</nav>
-	);
+  return (
+    <nav aria-label="pagination" className="nav-pagination">
+      <ul className={style["pagination"]}>{paginationLinks}</ul>
+    </nav>
+  );
 };
 export default Pagination;
