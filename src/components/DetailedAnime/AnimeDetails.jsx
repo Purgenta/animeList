e19c0ff666b2e63f28d5fd04eae0687cf25e174e
Purgenta <onlinenidza352@gmail.com>
2023-01-19 09:21:20 +0100

    Fixed an issue where a studio name wouldn't display properly

--- src/components/DetailedAnime/AnimeDetails.jsx
+++ src/components/DetailedAnime/AnimeDetails.jsx
@@ -1,4 +1,5 @@
 import style from "./AnimeDetails.module.css";
+import AnimeStats from "./AnimeStats";
 export default function DetailedAnime({ animeData }) {
   const {
     images: {
@@ -18,39 +19,20 @@ export default function DetailedAnime({ animeData }) {
     <section className="detailed-anime">
       <div className={style["primary-info"]}>
         <div className={style["hero-img__container"]}>
-          <img src={image_url} alt={`${title} cover`}></img>
+          <img
+            src={image_url}
+            className={style["hero-img"]}
+            alt={`${title} cover`}
+          ></img>
         </div>
-        <section className={style["stats"]}>
-          <div className={style["score-information"]}>
-            <h3>Score</h3>
-            <h2 className={style["score"]}>{score || "No score yet"}</h2>
-            {score && (
-              <p className={style["scored_by"]}>
-                {scored_by} <span>users</span>
-              </p>
-            )}
-          </div>
-          <div className="wrapper">
-            <ul className={style["rankings"]}>
-              <li>
-                <h3 className="stat">
-                  Ranked: <span>#{rank}</span>
-                </h3>
-              </li>
-              <li>
-                <h3 className="stat">
-                  Popularity: <span>#{popularity}</span>
-                </h3>
-              </li>
-              <li>
-                <h3 className="stat">Members: {members}</h3>
-              </li>
-            </ul>
-            <h4 className={style["studio-name"]}>
-              Studio: {studios[0]["name"]} {}
-            </h4>
-          </div>
-        </section>
+        <AnimeStats
+          score={score}
+          members={members}
+          rank={rank}
+          popularity={popularity}
+          studios={studios}
+          scored_by={scored_by}
+        ></AnimeStats>
       </div>
     </section>
   );
