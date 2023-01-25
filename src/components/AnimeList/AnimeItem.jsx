0c667bd776b9dbd2a9e4c35f7d63c740bffd68e9
Purgenta <onlinenidza352@gmail.com>
2023-01-25 13:25:21 +0100

    Grid List fix

--- src/components/AnimeList/AnimeItem.jsx
+++ src/components/AnimeList/AnimeItem.jsx
@@ -14,15 +14,20 @@ export default function AnimeItem({
       whileHover={{ backgroundColor: "#2c3636" }}
       className={style["anime-item"]}
     >
-      <Link to={`/anime/${mal_id}`} className={style["anime-image__container"]}>
-        <img
-          alt={`${title} cover`}
-          loading={shouldDefer ? "lazy" : "eager"}
-          src={`${image_url}`}
-        ></img>
-      </Link>
-      <AnimeGenre genres={genres}></AnimeGenre>
-      <h3 className={style["anime-item__title"]}>{title}</h3>
+      <div className={style["wrapper"]}>
+        <Link
+          to={`/anime/${mal_id}`}
+          className={style["anime-image__container"]}
+        >
+          <img
+            alt={`${title} cover`}
+            loading={shouldDefer ? "lazy" : "eager"}
+            src={`${image_url}`}
+          ></img>
+        </Link>
+        <AnimeGenre genres={genres}></AnimeGenre>
+        <h3 className={style["anime-item__title"]}>{title}</h3>
+      </div>
     </motion.li>
   );
 }
