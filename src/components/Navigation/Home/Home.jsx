3b64d66791f7168fd5268dcbd381d5b9e75b863b
Purgenta <105285149+Purgenta@users.noreply.github.com>
2023-02-18 16:37:09 +0100

    Update Home.jsx

--- src/components/Navigation/Home/Home.jsx
+++ src/components/Navigation/Home/Home.jsx
@@ -9,7 +9,7 @@ export default function Home(props) {
           <p className={style["website-information"]}>
             Thousands of anime and related content. Explore now!
             <br />
-            Developed by Nikola Todorovic and powered by JikanApi
+            Developed by Purgenta and powered by JikanApi
           </p>
         </div>
       </section>
